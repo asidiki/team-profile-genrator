27fabc7f0cdc661a19701ac327aad4121dc15152
Ansab Sidiki <ansab@sidiki.io>
2022-04-17 08:42:40 -0400

    completed employee.test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -9,3 +9,27 @@ test('creates employee object', ()=> {
     expect(employee.email).toEqual(expect.any(String));
 
 });
+
+//getName
+test('get employee name', ()=> {
+    const employee = new Employee('Ansab', 30, 'ansab@sidiki.io')
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+//getId
+test('get employee id', ()=> {
+    const employee = new Employee('Ansab', 30, 'ansab@sidiki.io')
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+//getEmail
+test('get employee email', ()=> {
+    const employee = new Employee('Ansab', 30, 'ansab@sidiki.io')
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+//getRole
+test('get employee role', ()=> {
+    const employee = new Employee('Ansab', 30, 'ansab@sidiki.io')
+    expect(employee.getRole()).toEqual('Employee');
+});
